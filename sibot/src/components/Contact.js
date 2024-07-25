d9d27d5e2faa9b8c706722d484a3cee9da370b40
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2024-07-25 12:43:48 +0500

    z fix

--- sibot/src/components/Contact.js
+++ sibot/src/components/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
             ></iframe>
           </div>
           <div className="container px-5 py-24 mx-auto flex">
-            <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+            <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative  shadow-md">
               <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
                 Feedback
               </h2>
